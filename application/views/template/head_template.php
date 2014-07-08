fd1282d7a97aac04e3cc3fa6486abb2a524af2e7
ChristophF <chrisatgit@outlook.com>
2014-07-08 21:28:27 +0200

    removing debug alert

--- application/views/template/head_template.php
+++ application/views/template/head_template.php
@@ -18,8 +18,7 @@
         <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.2.1.1.min.js"></script>
         <script type="text/javascript">
             var isMobile = false;
-            if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-                alert("mobil");
+            if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {  
                 isMobile = true;
             }
 
