0453a39c60c8507701c74c883682d538b428cc72
ChristophF <chrisatgit@outlook.com>
2014-07-09 22:48:19 +0200

    added plugin backup to system-controller

--- application/language/1/system_lang.php
+++ application/language/1/system_lang.php
@@ -1,11 +1,22 @@
 <?php
 //header
-$lang['system_header']              = 'System';
+$lang['system_header']              = 'System | Erweiterungen';
 
 //options
-$lang['system_option_backup']       = 'Sicherung von Erweiterungen';
+$lang['system_option_backup']       = 'Sicherung erstellen';
+$lang['system_option_installPlugin']= 'hinzufügen / aktualisieren';
 
 //hints
-$lang['system_hint_backup']         = 'Erstellung einer Sicherung einer Erweiterung. Dies sollte vor der Installation einer neuen Erweiterung oder einer Aktualisierung durchgeführt werden.'
+$lang['system_hint_backup']         = 'Erstellung einer Sicherung einer Erweiterung. Dies sollte vor der Installation einer neuen Erweiterung oder einer Aktualisierung durchgeführt werden.';
+$lang['system_hint_installPlugin']  = 'Spielen Sie hier eine neue Erweiterung ein oder aktualisieren Sie eine bereits bestehende. Aktualisierungen überschreiben Erweiterungen.';
+
+//buttons
+$lang['system_button_backup']       = 'Sicherung erstellen';
+$lang['system_button_installPlugin']= 'Erweiterung einspielen';
+
+//dialogs
+$lang['system_dialog_backupPlugin_dialogTitle']     = 'Sicherung erstellen';
+$lang['system_dialog_backupPlugin_dialogMessage']   = 'Wählen Sie eines der installierten Plugins und erstellen Sie anschließend eine Sicherung in Form einer *.zip-Datei auf dem Server.';
+$lang['system_dialog_backupPlugin_buttonLabel']     = 'Demo - schließen';
 
 ?>
--- application/views/system/system.php
+++ application/views/system/system.php
@@ -7,9 +7,23 @@
 <div class="row">
     <div class="col-xs-12 col-sm-4 col-md-3">
         <div class="panel panel-default">
-            <div class="panel-heading"><?php echo $this->lang->line('system_option_backup');?></div>
+            <div class="panel-heading">
+                <strong><?php echo $this->lang->line('system_option_backup');?></strong>
+            </div>
             <div class="panel-body">
                 <p style="text-align: justify;"><small><?php echo $this->lang->line("system_hint_backup"); ?></p></small></p>
+                <button class="btn btn-success" name="pluginBackup" data-dialogTitle="<?php echo $this->lang->line("system_dialog_backupPlugin_dialogTitle"); ?>" data-dialogMessage="<?php echo $this->lang->line("system_dialog_backupPlugin_dialogMessage"); ?>" data-buttonLabel="<?php echo $this->lang->line("system_dialog_backupPlugin_buttonLabel"); ?>" style="width: 100%;" type="button"><?php echo $this->lang->line('system_button_backup'); ?></button>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-4 col-md-3">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <strong><?php echo $this->lang->line('system_option_installPlugin');?></strong>
+            </div>
+            <div class="panel-body">
+                <p style="text-align: justify;"><small><?php echo $this->lang->line("system_hint_installPlugin"); ?></p></small></p>
+                <button class="btn btn-success" style="width: 100%;" type="button"><?php echo $this->lang->line('system_button_installPlugin'); ?></button>
             </div>
         </div>
     </div>
