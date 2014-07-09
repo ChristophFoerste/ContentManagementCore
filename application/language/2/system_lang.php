d27e911a315b31a4d8b85de87d19f57ead1397fb
ChristophF <chrisatgit@outlook.com>
2014-07-09 23:01:34 +0200

    updated language file for system plugin

--- application/language/2/system_lang.php
+++ application/language/2/system_lang.php
@@ -1,11 +1,22 @@
 <?php
 //header
-$lang['system_header']              = 'System';
+$lang['system_header']              = 'System | Plugins';
 
 //options
-$lang['system_option_backup']       = 'Plugin backups';
+$lang['system_option_backup']       = 'Create Backups';
+$lang['system_option_installPlugin']= 'add / update';
 
 //hints
-$lang['system_hint_backup']         = 'Create a backup of a special plugin. This is recommended before installing a new plugin version.'
+$lang['system_hint_backup']         = 'Create a backup of a special plugin. This is recommended before installing a new plugin version.';
+$lang['system_hint_installPlugin']  = 'Install or update an existing plugin. Plugins will be overwritten by new installation.';
+
+//buttons
+$lang['system_button_backup']       = 'create backup';
+$lang['system_button_installPlugin']= 'install plugin';
+
+//dialogs
+$lang['system_dialog_backupPlugin_dialogTitle']     = 'Create backup';
+$lang['system_dialog_backupPlugin_dialogMessage']   = 'Choose an installed plugin and create a *.zip-file on the server';
+$lang['system_dialog_backupPlugin_buttonLabel']     = 'Demo - close';
 
 ?>
